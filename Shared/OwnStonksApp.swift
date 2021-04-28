40606d004c1490433c7aea9cbea2499c3c6e0d8b
kamaal111 <kamaal.f1@gmail.com>
2021-04-28 21:52:07 +0200

    Initial commit

---
+++ Shared/OwnStonksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnStonksApp.swift
+//  Shared
+//
+//  Created by Kamaal M Farah on 28/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct OwnStonksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
