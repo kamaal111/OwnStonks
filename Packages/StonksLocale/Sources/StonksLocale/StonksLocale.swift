2eb0601dc1024338877297054c2ac2167a419811
kamaal111 <kamaal.f1@gmail.com>
2021-05-09 11:42:27 +0200

    Documenting StonksLocale

--- Packages/StonksLocale/Sources/StonksLocale/StonksLocale.swift
+++ Packages/StonksLocale/Sources/StonksLocale/StonksLocale.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/// StonksLocale contains keys to localize the app
 public struct StonksLocale {
     private init() { }
 
@@ -27,16 +28,22 @@ public struct StonksLocale {
 }
 
 extension StonksLocale.Keys {
+    /// Returns a localized string
     public var localized: String {
         localized(with: [])
     }
 
+    /// Returns a localized string with the variables provided
+    /// - Parameter variables: These variables are injected in to the localized string
+    /// - Returns: A localized string
     public func localized(with variables: CVarArg...) -> String {
         StonksLocale.getLocalizableString(of: self, with: variables)
     }
 }
 
 extension Text {
+    /// An alternative init to use Text with `StonksLocale Keys`
+    /// - Parameter localized: An localized key to get a localized `Text` view
     public init(localized: StonksLocale.Keys) {
         self.init(StonksLocale.getLocalizableString(of: localized))
     }
