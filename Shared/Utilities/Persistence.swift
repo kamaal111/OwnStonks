8195a4c77b45a5755887c6db98a705dd74093e09
Kamaal <kamaal@tranzer.com>
2021-05-11 19:12:53 +0200

    Creating a preview core data container

--- Shared/Utilities/Persistence.swift
+++ Shared/Utilities/Persistence.swift
@@ -43,35 +43,70 @@ struct PersistenceController {
 extension PersistenceController {
     static let preview: PersistanceManager = {
         let result = PersistenceController(inMemory: true).sharedInststance
-        let dummyTransactions: [DummyTransaction] = [
-        ]
-        if let context = result.context {
-            for transaction in dummyTransactions {
-                #error("Not finished")
-                let newTransaction = CoreTransaction(context: context)
-                newTransaction.costPerShare = transaction.costPerShare
-                newTransaction.createdDate = Date()
-            }
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        let dummyTransactions: [DummyTransaction]
+        do {
+            dummyTransactions = try decoder.decode([DummyTransaction].self, from: Self.dummyTransactions)
+        } catch {
+            fatalError("\(error.localizedDescription) ::: \(error)")
         }
-//        for _ in 0..<10 {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//        }
-//        do {
-//            try viewContext.save()
-//        } catch {
-//            // Replace this implementation with code to handle the error appropriately.
-//            let nsError = error as NSError
-//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//        }
+        guard let context = result.context else { fatalError("No context") }
+        for transaction in dummyTransactions {
+            let newTransaction = CoreTransaction(context: context)
+            newTransaction.costPerShare = transaction.costPerShare
+            newTransaction.createdDate = transaction.createdDate
+            newTransaction.id = transaction.id
+            newTransaction.name = transaction.name
+            newTransaction.shares = transaction.shares
+            newTransaction.symbol = transaction.symbol
+            newTransaction.transactionDate = transaction.transactionDate
+            newTransaction.updatedDate = transaction.updatedDate
+        }
+        do {
+            try context.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
         return result
     }()
 
-    struct DummyTransaction {
+    static let dummyTransactions = """
+    [
+        {
+            "id": "81da49ba-a560-4022-992e-0faef20a6a59",
+            "name": "Apple",
+            "cost_per_share: 132,
+            "transaction_date": "2021-05-11T17:04:10+0000",
+            "shares": 2,
+            "created_date": "2021-05-11T17:04:10+0000",
+            "symbol": "AAPL",
+            "updated_date": "2021-05-11T17:04:10+0000"
+        }
+    ]
+    """.data(using: .utf8)!
+
+    struct DummyTransaction: Codable {
+        let id: UUID
         let name: String
         let costPerShare: Double
         let transactionDate: Date
-        let shares: String
+        let shares: Double
+        let createdDate: Date
+        let symbol: String
+        let updatedDate: Date
+
+        enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case costPerShare = "cost_per_share"
+            case transactionDate = "transaction_date"
+            case shares
+            case createdDate = "created_date"
+            case symbol
+            case updatedDate = "updated_date"
+        }
     }
 }
 #endif
