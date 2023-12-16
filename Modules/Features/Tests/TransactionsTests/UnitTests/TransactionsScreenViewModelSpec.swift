4b5aad02459a4c43bbdda00a022859ea05a2b338
Kamaal Farah <kamaal.f1@gmail.com>
2023-12-16 16:50:14 +0100

    Testing showing details sheet

--- Modules/Features/Tests/TransactionsTests/UnitTests/TransactionsScreenViewModelSpec.swift
+++ Modules/Features/Tests/TransactionsTests/UnitTests/TransactionsScreenViewModelSpec.swift
@@ -7,6 +7,7 @@
 
 import Quick
 import Nimble
+import Foundation
 @testable import Transactions
 
 final class TransactionsScreenViewModelSpec: QuickSpec {
@@ -27,13 +28,21 @@ final class TransactionsScreenViewModelSpec: QuickSpec {
                 expect(viewModel.showSheet) == true
             }
 
+            it("should open transaction details sheet") {
+                // When
+                viewModel.handleTransactionPress(testTransaction)
+
+                expect(viewModel.shownSheet) == .transactionDetails(testTransaction)
+                expect(viewModel.showSheet) == true
+            }
+
             it("should have the correct state by default") {
                 // Then
                 expect(viewModel.shownSheet).to(beNil())
                 expect(viewModel.showSheet) == false
             }
 
-            it("should reset sheet state on sheet dismissal") {
+            it("should reset sheet state on sheet dismissal by setting showSheet to false") {
                 viewModel.showAddTransactionSheet()
 
                 // When
@@ -46,3 +55,13 @@ final class TransactionsScreenViewModelSpec: QuickSpec {
         }
     }
 }
+
+private let testTransaction = AppTransaction(
+    id: UUID(uuidString: "519c218b-79c6-4f13-b38b-d8c7b5d0d3f5")!,
+    name: "Shopify",
+    transactionDate: Date(timeIntervalSince1970: 1_702_741_633),
+    transactionType: .buy,
+    amount: 200,
+    pricePerUnit: Money(value: 500, currency: .CAD),
+    fees: Money(value: 3.2, currency: .USD)
+)
