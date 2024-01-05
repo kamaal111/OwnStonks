2e0a5b20ca20cc51218659e94f46dbb00d3245fb
Kamaal Farah <kamaal.f1@gmail.com>
2024-01-06 00:11:57 +0100

    Fetching data sources by transaction id

--- Modules/Features/Sources/Playground/Views/SupportingViews/CloudDatabase/PlaygroundCloudDatabaseDataRow.swift
+++ Modules/Features/Sources/Playground/Views/SupportingViews/CloudDatabase/PlaygroundCloudDatabaseDataRow.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 import CloudKit
+import KamaalPopUp
 
 struct PlaygroundCloudDatabaseDataRow: View {
+    @EnvironmentObject private var kPopUpManager: KPopUpManager
+
     let keys: [String]
     let record: CKRecord
 
     var body: some View {
         ForEach(uniqueKeys, id: \.self) { key in
             if let value = getValue(by: key) {
-                Text(value)
+                Button(action: { action(with: value) }) {
+                    Text(value)
+                }
+                .buttonStyle(.plain)
             } else {
                 Text("null")
                     .foregroundStyle(.secondary)
@@ -23,15 +29,42 @@ struct PlaygroundCloudDatabaseDataRow: View {
         }
     }
 
-    var uniqueKeys: [String] {
+    private var uniqueKeys: [String] {
         let id = record.recordID.recordName
         return keys.map { key in "\(id)#\(key)" }
     }
 
+    private func action(with value: String) {
+        Clipboard.copy(value)
+        kPopUpManager.showPopUp(
+            style: .hud(title: "Copied '\(value)'", systemImageName: "doc.on.clipboard", description: nil),
+            timeout: 4
+        )
+    }
+
     private func getValue(by key: String) -> String? {
         let key = key.split(separator: "#").dropFirst().joined()
+        if key == "recordName" {
+            return record.recordID.recordName
+        }
+
         guard let value = record[key] else { return nil }
         return "\(value as Any)"
+    }
+}
+
+struct Clipboard {
+    init() { }
+
+    static func copy(_ value: String) {
+        #if os(macOS)
+        let pasteboard = NSPasteboard.general
+        pasteboard.declareTypes([.string], owner: nil)
+        pasteboard.setString(value, forType: .string)
+        #else
+        let pasteboard = UIPasteboard.general
+        pasteboard.string = value
+        #endif
     }
 }
 
