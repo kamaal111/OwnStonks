9f99b0dc63b344af9f4cba686dc868ad234ccaae
Kamaal Farah <kamaal.f1@gmail.com>
2024-01-19 18:57:21 +0100

    Mapping previous closes immediatly

--- Modules/Features/Sources/Transactions/Internals/ViewModels/TransactionDetailsSheet+ViewModel.swift
+++ Modules/Features/Sources/Transactions/Internals/ViewModels/TransactionDetailsSheet+ViewModel.swift
@@ -268,7 +268,6 @@ extension TransactionDetailsSheet {
                 let closesResult = await stonksKit.tickers.closes(for: assetTicker, startDate: transactionDate)
                     .map { success -> ClosesData? in
                         let closesMappedByDates = success.closesMappedByDates
-
                         guard !closesMappedByDates.isEmpty else { return nil }
 
                         let currency = success.currency
