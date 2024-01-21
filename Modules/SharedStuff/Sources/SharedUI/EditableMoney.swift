212914f6d743ce7ae80e274ccee9659bbad13958
Kamaal Farah <kamaal.f1@gmail.com>
2024-01-21 21:37:06 +0100

    Hiding money in detail sheet

--- Modules/SharedStuff/Sources/SharedUI/EditableMoney.swift
+++ Modules/SharedStuff/Sources/SharedUI/EditableMoney.swift
@@ -16,12 +16,14 @@ public struct EditableMoney: View {
 
     public let label: String
     public let isEditing: Bool
+    public let show: Bool
 
-    public init(currency: Binding<Currencies>, value: Binding<String>, label: String, isEditing: Bool) {
+    public init(currency: Binding<Currencies>, value: Binding<String>, label: String, isEditing: Bool, show: Bool) {
         self._currency = currency
         self._value = value
         self.label = label
         self.isEditing = isEditing
+        self.show = show
     }
 
     public var body: some View {
@@ -35,11 +37,33 @@ public struct EditableMoney: View {
                 fixMessage: NSLocalizedString("Invalid value", bundle: .module, comment: "")
             )
         } else {
-            AppLabel(title: label, value: Money(value: Double(value) ?? 0, currency: currency).localized)
+            AppLabel(title: label, value: moneyLabel)
+                .foregroundColor(labelColor)
         }
+    }
+
+    private var labelColor: Color {
+        if !show {
+            return .secondary
+        }
+
+        return .primary
+    }
+
+    private var moneyLabel: String {
+        assert(!isEditing)
+        if !show {
+            return "***"
+        }
+
+        return money.localized
+    }
+
+    private var money: Money {
+        Money(value: Double(value) ?? 0, currency: currency)
     }
 }
 
 #Preview {
-    EditableMoney(currency: .constant(.AUD), value: .constant("200.0"), label: "Money", isEditing: false)
+    EditableMoney(currency: .constant(.AUD), value: .constant("200.0"), label: "Money", isEditing: false, show: true)
 }
